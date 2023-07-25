4fec37826d99e04757d6fa7787065a7d32ccbeb7
unknown <stefanjdavidov@gmail.com>
2023-07-25 19:52:38 -0400

    Fixed misspelling

--- src/CodingJourney.jsx
+++ src/CodingJourney.jsx
@@ -1,12 +1,12 @@
 import Header from "./Header";
 import Footer from "./Footer";
-import TimeLine from "./Timeline";
+import TimeLine from "./TimeLine";
 import PictureCollage from "./PictureCollage";
 
 export default function MostPopularRecipies() {
   return (
     <>
-      {document.body.style.overflow = ""}
+      {(document.body.style.overflow = "")}
       <Header title="Coding Journey" />
       <PictureCollage />
       <TimeLine />
